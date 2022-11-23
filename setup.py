5ce6a8014e6ce5fd42cb0d1dc998bec0ce4c689b
Utku Çam <utkucam2@gmail.com>
2022-11-23 03:08:02 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='finance-telegram-bot',
+    version='0.0.1',
+    packages=['utils', 'command_handlers'],
+    url='https://github.com/aykuttd/finance-telegram-bot',
+    license='',
+    author='Aykut Dere',
+    author_email='',
+    description=''
+)
